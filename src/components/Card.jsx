a5972345e5cb1e60be384bb4ba1ce462f6cb8d1a
VelyVi <182722202+VelyVi@users.noreply.github.com>
2024-11-12 20:56:07 -0600

    update/changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,9 +9,37 @@ function Card() {
 	const [location, setLocation, loading, error] = useFetch();
 	const [locationId, setLocationId] = useState(1);
 
+	const [page, setPage] = useState(1);
+	const itemsPerPage = 8;
+	const totalItems = location ? location?.residents.length : 0;
+	const maxPage = Math.ceil(totalItems / itemsPerPage);
+
 	useEffect(() => {
 		setLocation(`https://rickandmortyapi.com/api/location/${locationId}`);
 	}, [locationId]);
+
+	const onPrev = () => {
+		if (page > 1) {
+			setPage(page - 1);
+		}
+	};
+
+	const onNext = () => {
+		if (page < maxPage) {
+			setPage(page + 1);
+		}
+	};
+
+	const onClickPage = (newPage) => {
+		setPage(newPage);
+	};
+
+	const items = [];
+
+	for (let i = 1; i <= maxPage; i++) {
+		items.push(i);
+	}
+
 	return (
 		<div className="container">
 			{loading ? (
@@ -27,7 +55,32 @@ function Card() {
 				<>
 					<Search setLocationId={setLocationId} />
 					<InfoCard location={location} />
-					<ResidentsList residents={location?.residents} />
+					<div className="btns">
+						<button onClick={onPrev} disabled={page === 1}>
+							Previous
+						</button>
+						{items.map((item, index) => (
+							<button
+								key={index}
+								onClick={() => onClickPage(item)}
+								disabled={page === item}
+							>
+								{item}
+							</button>
+						))}
+						<button onClick={onNext} disabled={page === maxPage}>
+							Next
+						</button>
+					</div>
+					<p className="btns__info">
+						{page} / {maxPage}
+					</p>
+					<br></br>
+					<ResidentsList
+						residents={location?.residents}
+						page={page}
+						itemsPerPage={itemsPerPage}
+					/>
 				</>
 			)}
 		</div>
--- src/components/ResidentsList.jsx
+++ src/components/ResidentsList.jsx
@@ -1,10 +1,14 @@
 import ResidentCard from './ResidentCard';
 import './ResidentsList.css';
 
-function ResidentsList({ residents }) {
+function ResidentsList({ residents, page, itemsPerPage }) {
+	const currentPageItems = residents
+		? residents?.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+		: [];
+
 	return (
 		<div className="cards">
-			{residents?.map((resident) => {
+			{currentPageItems?.map((resident) => {
 				const residentSplit = resident.split('/');
 				const id = residentSplit[residentSplit.length - 1];
 
